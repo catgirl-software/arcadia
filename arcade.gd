0449c3e2bdc9f75c6018364a3368eebcc1afd79f
elle <elle@catgirl.software>
2022-11-27 09:53:37 +1030

    tweening, frames

--- arcade.gd
+++ arcade.gd
@@ -6,7 +6,9 @@ const CAMERA_X = 800
 const CAMERA_Y = 600
 const SHOP_X = 48*7
 const SHOP_Y = 32*7
-
+const STAIR_X = 16*7
+const FRAME_EDGE = 8*7
+const FRAME_Y = 72*7
 var customer_scene = preload("res://customer.tscn")
 
 var shops = {}
@@ -27,14 +29,20 @@ func _ready():
 	$camera.position.x = CAMERA_X/2 - SHOP_X/2
 	$camera.position.y = (-CAMERA_Y/2) + SHOP_Y/2
 	$camera/shopdetails.text = ""
+	$frame.position.x = -SHOP_X/2
+	$frame.position.y = SHOP_Y/2
 	print(self.get_children()[0])
 	for x in range(6):
 		shops[x] = {}
 		for y in range(2):
 			shops[x][y] = get_node("shop" + str(x) + str(y))
 			shops[x][y].set_location(location.new(x, y)).empty()
-			shops[x][y].position.x = SHOP_X * x
-			shops[x][y].position.y = -SHOP_Y * y
+			shops[x][y].position.x = SHOP_X * x + FRAME_EDGE
+			if x >=1:
+				shops[x][y].position.x += STAIR_X
+			if x >=5:
+				shops[x][y].position.x += STAIR_X
+			shops[x][y].position.y = -SHOP_Y * y - FRAME_EDGE
 			shops[x][y].get_node("Shopfront").frame = y
 	var leftStair = shop.new().set_location(location.new("stairs", "left")).stairs()
 	add_child(leftStair)
@@ -108,28 +116,27 @@ func tick_timer_cb():
 	for c in arcade_members:
 		if util.chance(0.5):
 			continue
-		var old_position = c.loc
 		var remove = process_action(c)
-		var new_position = c.loc
 		if remove != null:
 			to_remove.append(remove)
 
-		if new_position.x_pos is int and old_position.x_pos is int:
-			if new_position.x_pos > old_position.x_pos:
-				c.flip_h = false
-			elif new_position.x_pos < old_position.x_pos:
-				c.flip_h = true
-			else:
-				c.flip_h = util.pick([true, false])
+#		if new_position.x_pos is int and old_position.x_pos is int:
+#			if new_position.x_pos > old_position.x_pos:
+#				c.flip_h = false
+#			elif new_position.x_pos < old_position.x_pos:
+#				c.flip_h = true
+#			else:
+#				c.flip_h = util.pick([true, false])
 
 		if c.cur_action in [customer.Action.Moving, customer.Action.Leaving]:
 			c.z_index = rng.randi_range(2000, 4000)
 		if c.cur_action in [customer.Action.InShop, customer.Action.Browsing]:
 			c.z_index = rng.randi_range(1000, 2000)
-		var shop = get_shop_at(new_position)
-		var shop_loc = get_customer_position(shop.position.x, shop.position.y)
-		c.position.x = shop_loc[0]
-		c.position.y = shop_loc[1]
+		print([c.cur_action, c.next_location])
+		if c.cur_action in [customer.Action.Moving, customer.Action.Leaving]:
+			var shop = get_shop_at(c.next_location)
+			var shop_loc = get_customer_position(shop.position.x, shop.position.y)
+			c.move_towards(shop_loc[0], shop_loc[1])
 
 	if len(to_remove):
 		var new_customer_list = []
@@ -148,7 +155,6 @@ func get_customer_position(x: int, y: int):
 
 func process_action(c):
 	print(c)
-
 	match c.cur_action:
 		customer.Action.Moving:
 			if location.same(c.loc, c.destination):
@@ -169,13 +175,15 @@ func process_action(c):
 								return
 							else:
 								print(str(c) + " wasn't interested...")
-			c.loc = next_location(c.loc, c.destination)
+			c.loc = c.next_location
+			c.next_location =  next_location(c.loc, c.destination)
 			print("customer " + str(c) + " moving to " + str(c.loc))
 		customer.Action.Leaving:
 			if location.same(c.loc, c.destination):
 				print("customer " + str(c) + " has left the arcade...")
 				return c
-			c.loc = next_location(c.loc, c.destination)
+			c.loc = c.next_location
+			c.next_location =  next_location(c.loc, c.destination)
 			print("customer " + str(c) + " leaving, moving to " + str(c.loc))
 		customer.Action.Browsing:
 			print("customer " + str(c) + " browsing...")
@@ -253,6 +261,7 @@ func new_customer(friendliness: int):
 			destination.loc,
 			n
 		)
+		c.next_location =  next_location(c.loc, c.destination)
 		add_child(c)
 		print(n + " is buying " + desire + " from " + str(destination))
 		arcade_members.append(c)
@@ -329,6 +338,11 @@ func left_stairs() -> location:
 	return location.new("left", "stairs")
 func right_stairs() -> location:
 	return location.new("right", "stairs")
+
+func left_exit() -> location:
+	return location.new("left", "exit")
+func right_exit() -> location:
+	return location.new("right", "exit")
 
 func left_entrance() -> location:
 	return location.new(0, 0)
--- customer.gd
+++ customer.gd
@@ -19,6 +19,10 @@ var visited_locations = []
 var money: float
 var sprite: Sprite
 
+var next_location: location
+var moving: bool
+var tween: SceneTreeTween
+
 func init(f: int, m: int, wants: String, l: location, dest: location, n: String):
 	frame = f
 	money = m
@@ -30,6 +34,12 @@ func init(f: int, m: int, wants: String, l: location, dest: location, n: String)
 	destination = dest
 	customer_name = n
 	cur_action = Action.Moving
+
+func move_towards(x: int, y: int):
+	if tween:
+		tween.kill()
+	tween = create_tween()
+	var _tweener = tween.tween_property(self, "position", Vector2(x, y), 1)
 
 func _to_string():
 	return customer_name + ", looking for " + desire + " at " + str(destination)
